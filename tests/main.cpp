e802230f032f1e8fdcb96a41afd96589d1621c91
Pradeep Ramani <pradeepramni@gmail.com>
2020-06-07 03:33:31 -0700

    minor

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,10 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
-#include "Register.h"
-#include "Combo.h"
-#include "Ports.h"
 
-#include <Module.h>
+#include <ClockSim.h>
 
 bool Simulate() {
     
